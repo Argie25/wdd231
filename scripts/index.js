b7951dcae42e00a6daafe31c0407875583ec9fba
Argie <argiedc888@gmail.com>
2024-11-13 10:30:00 +0800

    parcially finished my home page

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+// Select the <span> element with id="currentyear"
+const currentYearElement = document.getElementById("currentyear");
+
+// Get the current year using the Date object
+const currentYear = new Date().getFullYear();
+
+// Populate the <span> element with the current year
+currentYearElement.textContent = currentYear;
+
+
+// Select the element
+const lastModifiedElement = document.getElementById("lastModified");
+
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+
+// Populate the element
+lastModifiedElement.textContent = `Last updated: ${lastModifiedDate}`;
